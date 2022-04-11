81322ac877376b7782a2d160a61b5857c6b8f876
박정현 <97150068+qkraksehs@users.noreply.github.com>
2022-04-11 12:43:30 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@
 // mobile toggle menu
 $(function(){
     $('#gnb-toggle-btn').on('click', function(){
-        $('#gnb').toggle('')
+        $('#gnb').toggleClass('on');
     })
-    // $('#gnb > ul > li > a').click(function(){
-    //     $('#gnb').hide();
-    // })
 });
+// loading screen
+window.onload = function() {
+    // 로딩중 지연시간
+    setTimeout(function(){
+        // console.log('3초가 경과함...')
+        $('.loading').removeClass('on');
+        // console.log('문서 로딩 완료!')
+    },1300)
+}
