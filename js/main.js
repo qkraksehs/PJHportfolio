6ac4af4df9547f88d8e9888480e437ac834388cd
박정현 <97150068+qkraksehs@users.noreply.github.com>
2022-04-12 10:30:33 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 // main.js
 // mobile toggle menu
 $(function(){
-    $('#gnb-toggle-btn').on('click', function(){
+    $('.ham').on('click', function(){
         $('#gnb').toggleClass('on');
     })
 });
+$(function(){
+    $(".ham").click(function(){
+        $(this).toggleClass("active")
+      });
+})
 // loading screen
 window.onload = function() {
     // 로딩중 지연시간
