251aab4c7c7c2798f680acb1c0bddfa7975d739f
박정현 <97150068+qkraksehs@users.noreply.github.com>
2022-04-25 20:54:07 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(function(){
     $(".ham").click(function(){
         $(this).toggleClass("active")
       });
-})
+});
 // loading screen
 window.onload = function() {
     // 로딩중 지연시간
@@ -18,7 +18,7 @@ window.onload = function() {
         $('.loading').removeClass('on');
         // console.log('문서 로딩 완료!')
     },1300)
-}
+};
 
 // mobile에서 pc로 넘어가도 gnb안없어지게..
 $(window).resize(function(){
@@ -26,4 +26,27 @@ $(window).resize(function(){
     if(innerWidth >= 769) {
         $('#gnb').show();
     }
-})
+});
+
+// 보고있는 영역 메뉴 강조 기능
+$(window).on('scroll', function () {
+ 
+    var pos = $(window).scrollTop();
+    var pos2 = pos + 50;
+    var scrollBottom = pos + $(window).height();
+ 
+    // Link Highlighting
+    if (pos2 > $('#page-1').offset().top)   { highlightLink('page-1'); }
+    if (pos2 > $('#page-2').offset().top) { highlightLink('page-2'); }
+    if (pos2 > $('#page-3').offset().top)  { highlightLink('page-3'); }
+    if (pos2 > $('#page-4').offset().top ||
+        pos + $(window).height() === $(document).height()) {
+          highlightLink('page-4');
+    }
+ 
+  });
+ 
+  function highlightLink(anchor) {
+    $('nav .active').removeClass('active');
+    $("nav").find('[href="#' + anchor + '"]').addClass('active');
+  }
